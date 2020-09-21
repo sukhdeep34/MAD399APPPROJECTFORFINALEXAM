b55aa9b94de5dc82cc833bb575d3680b9b7fded0
sukhdeep34 <64862362+sukhdeep34@users.noreply.github.com>
2020-09-21 17:15:19 -0400

    my new app

--- app/src/main/java/com/example/my1stmobileapplication/Login.java
+++ app/src/main/java/com/example/my1stmobileapplication/Login.java
@@ -3,12 +3,25 @@ package com.example.my1stmobileapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    EditText mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn;
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.Email);
+        
     }
 }
